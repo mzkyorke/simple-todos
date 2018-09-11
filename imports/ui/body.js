5d8b138a3f689ced2244a486ba37b91091cf7878
mkyorke <mzkyorke@yahoo.com>
2018-09-11 11:04:55 +0000

    Body Works

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,23 +1,13 @@
 import { Template } from 'meteor/templating';
 
- 
-
 import { Tasks } from '../api/tasks.js';
-
- 
 
 import './body.html';
 
- 
-
 Template.body.helpers({
-
   tasks() {
-
     return Tasks.find({});
-
   },
-
 });
 
 
